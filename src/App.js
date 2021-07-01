3a81711b212ae90889937052ef7988af84a5f686
Prakash Ravichandran <prakash.ravichandran@hashouttech.com>
2021-07-01 18:55:15 +0530

    added CustomActiveShapePieChart

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import renderLineChart from './components/charts.components';
 import Example from './components/charts.components';
+import Example2 from './components/chartsProject.component';
+import CustomActiveShapePieChart from './components/CustomActiveShapePieChart';
 
 function App() {
   return (
        <div>
          <h1>hello prakash</h1>
     <Example/>
+    <Example2/>
+    <CustomActiveShapePieChart/>
        </div>
        
   );
